7c3a4e9ca5c58fa83e4683c410ab44104a328919
hotio <26902309+hotio@users.noreply.github.com>
2020-08-09 04:30:42 +0200

    switch to gha

--- linux-arm.Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM hotio/base@sha256:cac4e16e96664e8f25578b009d4c7471be2e04937e943a16a560adea042e843b
-
-EXPOSE 7337
-
-ARG TRACKARR_URL
-RUN curl -fsSL "${TRACKARR_URL}" | tar xzf - -C "${APP_DIR}" && \
-    chmod -R u=rwX,go=rX "${APP_DIR}" && chmod 755 "${APP_DIR}/trackarr"
-
-COPY root/ /
--- update.sh
+++ update.sh
@@ -1,54 +1,14 @@
 #!/bin/bash
 
-if [[ ${1} == "screenshot" ]]; then
-    SERVICE_IP="http://$(ping -c1 -4 service | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'):7337"
-    NETWORK_IDLE="2"
-    cd /usr/src/app && node <<EOF
-const puppeteer = require('puppeteer');
-
-(async () => {
-  const browser = await puppeteer.launch({
-    bindAddress: "0.0.0.0",
-    args: [
-      "--no-sandbox",
-      "--headless",
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-      "--remote-debugging-port=9222",
-      "--remote-debugging-address=0.0.0.0"
-    ]
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
-  try {
-    await page.goto("${SERVICE_IP}", { waitUntil: "networkidle${NETWORK_IDLE}" });
-  } catch (e) {
-    console.log(e)
-    process.exit(1)
-  }
-  await page.evaluate(() => {
-    const div = document.createElement('div');
-    div.innerHTML = 'Image: ${DRONE_REPO_OWNER}/${DRONE_REPO_NAME##docker-}:${DRONE_COMMIT_BRANCH}<br>App Version: ${VERSION_FIELD}<br>Commit: ${DRONE_COMMIT_SHA:0:7}<br>Build: #${DRONE_BUILD_NUMBER}<br>Timestamp: $(date -u --iso-8601=seconds)';
-    div.style.cssText = "all: initial !important; border-radius: 4px !important; font-weight: normal !important; font-size: normal !important; font-family: monospace !important; padding: 10px !important; color: black !important; position: fixed !important; bottom: 10px !important; right: 10px !important; background-color: #e7f3fe !important; border-left: 6px solid #2196F3 !important; z-index: 10000 !important";
-    document.body.appendChild(div);
-  });
-  await page.screenshot({ path: "/drone/src/screenshot.png", fullPage: true });
-  await browser.close();
-})();
-EOF
-elif [[ ${1} == "checkservice" ]]; then
-    SERVICE="http://service:7337"
-    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL ${SERVICE} > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
-    curl -fsSL ${SERVICE} > /dev/null
-elif [[ ${1} == "checkdigests" ]]; then
+if [[ ${1} == "checkdigests" ]]; then
     mkdir ~/.docker && echo '{"experimental": "enabled"}' > ~/.docker/config.json
     image="hotio/base"
     tag="alpine"
     manifest=$(docker manifest inspect ${image}:${tag})
     [[ -z ${manifest} ]] && exit 1
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm.Dockerfile   && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
+    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
 else
     data=$(curl -fsSL https://gitlab.com/api/v4/projects/cloudb0x%2Ftrackarr/releases | jq -r '.[] | select (.name | endswith("-dev") | not)' | jq -s '.[0]')
     version=$(echo "${data}" | jq -r '.name' | sed s/^v//g)
@@ -57,9 +17,9 @@ else
     url_amd64=$(echo "${data}" | jq -r '.assets.links[] | select (.name | contains("linux_amd64.tar.gz")).url')
     url_arm=$(echo "${data}" | jq -r '.assets.links[] | select (.name | contains("linux_arm.tar.gz")).url')
     url_arm64=$(echo "${data}" | jq -r '.assets.links[] | select (.name | contains("linux_arm64.tar.gz")).url')
-    sed -i "s/{TRACKARR_VERSION=[^}]*}/{TRACKARR_VERSION=${version}}/g" .drone.yml
-    sed -i "s#{TRACKARR_URL_AMD64=[^}]*}#{TRACKARR_URL_AMD64=${url_amd64}}#g" .drone.yml
-    sed -i "s#{TRACKARR_URL_ARM=[^}]*}#{TRACKARR_URL_ARM=${url_arm}}#g" .drone.yml
-    sed -i "s#{TRACKARR_URL_ARM64=[^}]*}#{TRACKARR_URL_ARM64=${url_arm64}}#g" .drone.yml
+    sed -i "s/{TRACKARR_VERSION=[^}]*}/{TRACKARR_VERSION=${version}}/g" .github/workflows/build.yml
+    sed -i "s#{TRACKARR_URL_AMD64=[^}]*}#{TRACKARR_URL_AMD64=${url_amd64}}#g" .github/workflows/build.yml
+    sed -i "s#{TRACKARR_URL_ARM=[^}]*}#{TRACKARR_URL_ARM=${url_arm}}#g" .github/workflows/build.yml
+    sed -i "s#{TRACKARR_URL_ARM64=[^}]*}#{TRACKARR_URL_ARM64=${url_arm64}}#g" .github/workflows/build.yml
     echo "##[set-output name=version;]${version}"
 fi
